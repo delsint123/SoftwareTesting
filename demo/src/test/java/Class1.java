a9181ce34885216b702f8ad7a1fc7c08a8418f00
Delphine Sintamour <delsint123@gmail.com>
2024-04-09 14:42:00 -0400

    testing

---
+++ demo/src/test/java/Class1.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class Class1 {
+    @Test
+    void method1() {
+        System.out.println("This is method 1");
+    }
+
+    @Test
+    void method2() {
+        System.out.println("This is method 2");
+    }
+
+    @Test
+    void method3() {
+        System.out.println("This is method 3");
+    }
+}
