039347cb66e6124ded37be80257fa7ff9f3efbf5
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-04-10 20:24:22 -0400

    added server clicking and redirect

--- demo/src/test/java/Discover.java
+++ demo/src/test/java/Discover.java
@@ -28,36 +28,42 @@ public class Discover {
     }
 
     @Test
-    public void testDiscoverLinks() {
-
-        // Servers link
+    public void testDiscoverLinks() throws InterruptedException {
         driver.findElement(By.xpath("//a[normalize-space()='Discover']")).click();
+        Thread.sleep(2000);
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://discord.com/servers");
         driver.navigate().back();
+
+        Thread.sleep(2000);
 
         WebElement searchField = driver.findElement(By.xpath("//input[@placeholder='Explore communities']"));
         searchField.sendKeys("Valorant");
         WebElement searchButton = driver.findElement(By.cssSelector("button.searchButton-2-uL9p"));
         searchButton.click();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Thread.sleep(5000);
+
+        List<WebElement> serverList = driver.findElements(By.cssSelector(".searchListItem-3mtFl3"));
+
+        for (WebElement server : serverList) {
+            String serverName = server.findElement(By.cssSelector(".listName-GkVhEs")).getText();
+
+            if ("VALORANT LFG".equalsIgnoreCase(serverName)) {
+                server.click();
+                Thread.sleep(5000);
+                driver.navigate().back();
+                Thread.sleep(5000);
+                break;
+            }
         }
 
         List<WebElement> tabs = driver.findElements(By.xpath("//div[contains(@class, 'categoryListItemContainer-')]"));
 
         for (WebElement tab : tabs) {
             tab.click();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Thread.sleep(5000);
         }
-
     }
 
 }
